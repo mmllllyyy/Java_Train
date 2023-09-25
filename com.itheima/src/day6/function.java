0127242d8b14a03e349093f19c2ad0aa493d59a8
mly <mmllllyyy2019@163.com>
2023-09-25 14:31:37 +0800

    day6q1

---
+++ com.itheima/src/day6/function.java
@@ -0,0 +1,24 @@
+package day6;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+import static day5.ArrayTrain.q3;
+
+public class function {
+
+    public static void main(String[] args) {
+        System.out.println("q1 = " + q1(0.5, 23));
+        System.out.println("q3(10,20,30) = " + q3(10, 20, 30));
+
+    }
+
+    private static double q1(double a, double b) {
+        return Math.min(a, b);
+    }
+
+
+    private static int q3(int a, int b, int c) {
+        Queue<Integer> q = new PriorityQueue<>(cmp);
+    }
+}
